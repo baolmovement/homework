5d42e51fbe789c49435faa755cbda8203483ce26
Andrew Bao <bao.andrew@gmail.com>
2018-07-15 13:11:27 -0700

    got all routes and controllers working, besides destroy

--- haikuRouter.js
+++ haikuRouter.js
@@ -4,12 +4,11 @@ const
     router = express.Router();
 
 router.get('/', Haikus.index)
-// router.get('/:id', Haikus.show)
-// router.get('/new', Haikus.new)
-// router.post('/:id', Haikus.update)
+router.get('/:id', Haikus.show)
+router.post('/:id', Haikus.update)
 router.post('/', Haikus.new) 
-// router.patch('/:id', Haikus.update)
-// router.delete('/:id', Haikus.destroy)
+router.patch('/:id', Haikus.update)
+router.delete('/:id', Haikus.destroy)
     
 
 module.exports = router;
