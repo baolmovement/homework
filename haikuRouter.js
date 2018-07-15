0eb4ad0bdcf1994f8f388d058267a98022c7137d
Andrew Bao <bao.andrew@gmail.com>
2018-07-15 13:24:18 -0700

    added comments and cleaned code

--- haikuRouter.js
+++ haikuRouter.js
@@ -1,14 +1,14 @@
 const 
-    Haikus = require('./haikusCtrl'),
+    Haikus = require('./haikusCtrl'),//Access to methods in controller
     express = require('express'),
     router = express.Router();
 
-router.get('/', Haikus.index)
-router.get('/:id', Haikus.show)
-router.post('/:id', Haikus.update)
-router.post('/', Haikus.new) 
-router.patch('/:id', Haikus.update)
-router.delete('/:id', Haikus.destroy)
+router.get('/', Haikus.index)//goes to Haikus controller and accesses method called index
+router.get('/:id', Haikus.show)//goes to Haikus controller and accesses method called show
+router.post('/:id', Haikus.update)//goes to Haikus controller and accesses method called update
+router.post('/', Haikus.new) //goes to Haikus controller and accesses method called new
+router.patch('/:id', Haikus.update)//goes to Haikus controller and accesses method called update
+router.delete('/:id', Haikus.destroy)//goes to Haikus controller and accesses method called destroy
     
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const
     port = 3000,
     logger = require('morgan'), 
     mongoose = require('mongoose'),
-    haikuRouter = require('./haikuRouter')
+    haikuRouter = require('./haikuRouter')//Access to routes/router file
 
 mongoose.connect("mongodb://localhost/hwk", (err) => {
     console.log(err || "Connected!")
-}) 
+}, { useNewUrlParser: true }) 
 
 //mid-ware
 app.use(logger('dev')) 
 app.use(express.json()) 
 
 //routes
-app.use('/', haikuRouter)
+app.use('/', haikuRouter)//express using routes from a seperate file called HaikuROuter.js
 
 app.listen(port, (err) => {
     console.log(err || `Server running on ${port}.`)
